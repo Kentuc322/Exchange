e04b81722cb290beaee6e1f60306a0afa48b1017
Kentuc322 <kenichirowada0322@gmail.com>
2026-01-29 15:41:54 +0900

    モータードライバーを使うのを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include "motors.hpp"
 
 void setup() {
-    
+    auto motor1 = MotorController(5, 0); // ピン5、チャンネル0のモーターコントローラーを作成
+    auto motor2 = MotorController(6, 1); // ピン6、チャンネル1のモーターコントローラーを作成
+    auto motor3 = MotorController(7, 2); // ピン7、チャンネル2のモーターコントローラーを作成
+    motor1.begin(); // モーターコントローラーの初期化
+    motor2.begin(); // モーターコントローラーの初期化
+    motor3.begin(); // モーターコントローラーの初期化
 }
 
 void loop() {
