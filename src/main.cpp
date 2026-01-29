b408f64f9257347d943a6c6513da9ee99f4a7b19
Kentuc322 <kenichirowada0322@gmail.com>
2026-01-29 15:53:26 +0900

    外に出した

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include "motors.hpp"
 #include "sensors.hpp"
 
+Sensors sensors;
+
+auto motor1 = MotorController(5, 0); // create motor controller: pin 5, channel 0
+auto motor2 = MotorController(6, 1); // create motor controller: pin 6, channel 1
+auto motor3 = MotorController(7, 2); // create motor controller: pin 7, channel 2
+
 void setup() {
-    Sensors sensors;
     sensors.begin(A0); // initialize sensors with moisture sensor pin A0
-    auto motor1 = MotorController(5, 0); // create motor controller: pin 5, channel 0
-    auto motor2 = MotorController(6, 1); // create motor controller: pin 6, channel 1
-    auto motor3 = MotorController(7, 2); // create motor controller: pin 7, channel 2
     motor1.begin(); // initialize motor controller
     motor2.begin(); // initialize motor controller
     motor3.begin(); // initialize motor controller
